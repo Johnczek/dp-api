70391169ad7b4934edb153404f584977a373cfcf
johnczek <janhrboticky@gmail.com>
2020-12-20 17:44:24 +0100

    Docker and security init commit

---
+++ src/main/java/cz/johnczek/dpapi/security/configuration/RestSecurityConfiguration.java
@@ -0,0 +1,20 @@
+package cz.johnczek.dpapi.security.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class RestSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                //no authentication needed for these context paths
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
+}
---
+++ src/main/java/cz/johnczek/dpapi/user/UserController.java
@@ -0,0 +1,13 @@
+package cz.johnczek.dpapi.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "Hello";
+    }
+}

8b2c017f18c6e5023eb5c123f3d3c3e698edb941
johnczek <janhrboticky@gmail.com>
2020-12-20 16:28:04 +0100

    Initial commit

---
+++ src/main/java/cz/johnczek/dpapi/DpApiApplication.java
@@ -0,0 +1,13 @@
+package cz.johnczek.dpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cz/johnczek/dpapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cz.johnczek.dpapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DpApiApplication.class);
+	}
+
+}

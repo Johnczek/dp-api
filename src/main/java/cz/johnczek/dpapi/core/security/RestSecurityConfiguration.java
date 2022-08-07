3e0ba79d0567564dad757d08285fc5bb69528d7e
Jan Hrboticky <janhrboticky@gmail.com>
2022-08-07 20:47:48 +0200

    Adding cors support

--- src/main/java/cz/johnczek/dpapi/core/security/RestSecurityConfiguration.java
+++ src/main/java/cz/johnczek/dpapi/core/security/RestSecurityConfiguration.java
@@ -6,6 +6,7 @@ import cz.johnczek.dpapi.user.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
